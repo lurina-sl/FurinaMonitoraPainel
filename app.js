0b61a6e283f21d937b831bb838ed7a0193ccb2d7
Lurina (.) <hebertdavidhebertcaladodebarro@gmail.com>
2026-06-07 11:16:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,109 @@
-// PINTE AQUI O LINK DO SEU BOT NO RAILWAY (Quando você gerar ele no passo abaixo)
-const LINK_DO_BOT_RAILWAY = "presence-notify.up.railway.app"; 
+const LINK_DO_BOT_RAILWAY = "presence-notify.up.railway.app";
 
 const socket = io(LINK_DO_BOT_RAILWAY);
 
 const input = document.getElementById("mensagemInput");
-const botao = document.getElementById("enviarBtn");
+const enviarBtn = document.getElementById("enviarBtn");
 
-function enviarMensagem() {
-    const texto = input.value;
-    if (texto.trim() !== "") {
-        // Envia a mensagem para o bot através da internet
-        socket.emit("enviarParaDiscord", { mensagem: texto });
-        input.value = ""; // Limpa a caixinha
-    }
+const messages =
+document.getElementById("messages");
+
+function abrirChat(){
+
+document.getElementById("chatPage")
+.style.display = "block";
+
+document.getElementById("voicePage")
+.style.display = "none";
+
 }
 
-botao.addEventListener("click", enviarMensagem);
-input.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") enviarMensagem();
+function abrirVoice(){
+
+document.getElementById("chatPage")
+.style.display = "none";
+
+document.getElementById("voicePage")
+.style.display = "block";
+
+}
+
+function enviarMensagem(){
+
+const texto = input.value;
+
+if(!texto.trim()) return;
+
+socket.emit("enviarParaDiscord",{
+    mensagem:texto
+});
+
+input.value = "";
+
+}
+
+enviarBtn.addEventListener(
+"click",
+enviarMensagem
+);
+
+input.addEventListener(
+"keypress",
+e=>{
+    if(e.key==="Enter")
+        enviarMensagem();
+}
+);
+
+socket.on("novaMensagem", msg => {
+
+const div =
+document.createElement("div");
+
+div.className = "message";
+
+div.innerHTML = `
+<b>${msg.usuario}</b><br>
+${msg.conteudo}
+`;
+
+messages.appendChild(div);
+
+messages.scrollTop =
+messages.scrollHeight;
+
+});
+
+document
+.getElementById("joinVoice")
+.addEventListener("click",()=>{
+
+socket.emit("joinVoice",{
+channelId:
+document.getElementById("voiceChannel")
+.value
+});
+
+});
+
+document
+.getElementById("leaveVoice")
+.addEventListener("click",()=>{
+
+socket.emit("leaveVoice");
+
+});
+
+document
+.getElementById("speakBtn")
+.addEventListener("click",()=>{
+
+socket.emit("ttsSpeak",{
+
+texto:
+document.getElementById("ttsText")
+.value
+
+});
+
 });
